911547d058e1f28c36e21deaba1afe6bca01bae3
James <contact@jhilton.co.uk>
2016-10-08 20:18:27 +0100

    changes

--- createboard.js
+++ createboard.js
@@ -24,22 +24,33 @@ var testgame = new Game("test1","test2");
 var board = new jimp.read("Images/DraughtsBoard.png");
 var red = new jimp.read("Images/RedDraughtsPeice.png");
 var black = new jimp.read("Images/BlackDraughtsPeice.png");
+var blackKing = new jimp.read("Images/BlackDraughtsPeiceKing.png");
+var redKing = new jimp.read("Images/RedDraughtsPeiceKing.png");
 
 
-Promise.all([board,red,black]).then(function(i){
+
+Promise.all([board,red,black,redKing,blackKing]).then(function(i){
 	var tmpBoard = i[0];
 	
-	for(var y = 0; y <= 9; y++) {
-		for(var x = 0;x <= 9;x++) {
-			if(testgame.board[x,y] == 'r') {
-				tmpBoard.composite(i[1],x*25,y*25);
+	
+	for(var x = 0; x <= 9; x++) {
+		for(var y = 0;y <= 9;y++) {
+			if(testgame.board[x][y] == 'r') {
+				tmpBoard.composite(i[1],y*25,x*25);
 			}
-			if(testgame.board[x,y] == 'b') {
-				tmpBoard.composite(i[2],x*25,y*25);
+			if(testgame.board[x][y] == 'b') {
+				tmpBoard.composite(i[2],y*25,x*25);
 			}
+			if(testgame.board[x][y] == 't') {
+				tmpBoard.composite(i[3],y*25,x*25);
+			}
+			if(testgame.board[x][y] == 'n') {
+				tmpBoard.composite(i[4],y*25,x*25);
+			}
+			
 			
 		}
 	}
 	
-	tmpBoard.write("newBoard.jpg");
+	tmpBoard.write("newBoard2.jpg");
 });
